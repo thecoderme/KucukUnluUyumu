3d47e1860e8c241ef542e21f01e9f384bea619b7
thecoderme <yasin.badur.nk@gmail.com>
2017-10-10 22:39:08 +0300

    Initial commit

---
+++ KucukUnluUyumu/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace KucukUnluUyumu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt_kelimeGir = new System.Windows.Forms.TextBox();
+            this.btn_kucukUnluUyumuKontrolEt = new System.Windows.Forms.Button();
+            this.Txt_Durum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(14, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kelime Gir:";
+            // 
+            // Txt_kelimeGir
+            // 
+            this.Txt_kelimeGir.Font = new System.Drawing.Font("Calibri", 12F);
+            this.Txt_kelimeGir.Location = new System.Drawing.Point(123, 32);
+            this.Txt_kelimeGir.Name = "Txt_kelimeGir";
+            this.Txt_kelimeGir.Size = new System.Drawing.Size(334, 32);
+            this.Txt_kelimeGir.TabIndex = 1;
+            this.Txt_kelimeGir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_kelimeGir_KeyDown);
+            // 
+            // btn_kucukUnluUyumuKontrolEt
+            // 
+            this.btn_kucukUnluUyumuKontrolEt.Font = new System.Drawing.Font("Calibri", 12F);
+            this.btn_kucukUnluUyumuKontrolEt.Location = new System.Drawing.Point(225, 70);
+            this.btn_kucukUnluUyumuKontrolEt.Name = "btn_kucukUnluUyumuKontrolEt";
+            this.btn_kucukUnluUyumuKontrolEt.Size = new System.Drawing.Size(232, 68);
+            this.btn_kucukUnluUyumuKontrolEt.TabIndex = 2;
+            this.btn_kucukUnluUyumuKontrolEt.Text = "Küçük Ünlü Uyumu Kontrol Et";
+            this.btn_kucukUnluUyumuKontrolEt.UseVisualStyleBackColor = true;
+            this.btn_kucukUnluUyumuKontrolEt.Click += new System.EventHandler(this.Btn_kucukUnluUyumuKontrolEt_Click);
+            // 
+            // Txt_Durum
+            // 
+            this.Txt_Durum.BackColor = System.Drawing.Color.White;
+            this.Txt_Durum.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Txt_Durum.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txt_Durum.Location = new System.Drawing.Point(123, 144);
+            this.Txt_Durum.Multiline = true;
+            this.Txt_Durum.Name = "Txt_Durum";
+            this.Txt_Durum.ReadOnly = true;
+            this.Txt_Durum.Size = new System.Drawing.Size(334, 90);
+            this.Txt_Durum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sonuç:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_kucukUnluUyumuKontrolEt;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 246);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txt_Durum);
+            this.Controls.Add(this.btn_kucukUnluUyumuKontrolEt);
+            this.Controls.Add(this.Txt_kelimeGir);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Küçük Ünlü Uyumu Ödevi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt_kelimeGir;
+        private System.Windows.Forms.Button btn_kucukUnluUyumuKontrolEt;
+        private System.Windows.Forms.TextBox Txt_Durum;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KucukUnluUyumu/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using net.zemberek.erisim;
+using net.zemberek.islemler;
+using net.zemberek.islemler.cozumleme;
+using net.zemberek.yapi;
+using net.zemberek.tr.yapi;
+
+namespace KucukUnluUyumu
+{
+    public partial class Form1 : Form
+    {
+        Zemberek zemberek = new Zemberek(new TurkiyeTurkcesi());
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+        private Sonuc KelimeKontrol(String Kelime)
+        {
+            Sonuc sonuc = new Sonuc();
+
+            Heceleyici heceleyici = zemberek.heceleyici();
+            Boolean b = heceleyici.hecelenebilirmi(Kelime);
+            if (!zemberek.kelimeDenetle(Kelime))
+            {
+                sonuc.msg = (Kelime + " kelimesi dogru yazılmamış.");
+                sonuc.sonuc = false;
+            }
+            else if (!b)
+            {
+                sonuc.msg = (Kelime + " kelimesi hecelenemez.");
+                sonuc.sonuc = false;
+            }
+            else
+            {
+                sonuc.msg = "";
+                sonuc.sonuc = true;
+            }
+            return sonuc;
+        }
+        private void Btn_kucukUnluUyumuKontrolEt_Click(object sender, EventArgs e)
+        {
+            if(Txt_kelimeGir.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Kelime giriniz.");
+                return;
+            }else if(Txt_kelimeGir.Text.Trim().Length >0 && Txt_kelimeGir.Text.Trim().Contains(" "))
+            {
+                MessageBox.Show("Kelime boşluk içeremez giriniz.");
+                return;
+            }
+            Sonuc sonuc = KucukUnluUyumunaBak(Txt_kelimeGir.Text.Trim());
+            if (sonuc.sonuc)
+                Txt_Durum.ForeColor = Color.Green;
+            else
+                Txt_Durum.ForeColor = Color.Red;
+
+            Txt_Durum.Text =sonuc.msg;
+        }
+
+        private void Txt_kelimeGir_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Sonuc sonuc = KucukUnluUyumunaBak(Txt_kelimeGir.Text);
+                if (sonuc.sonuc)
+                    Txt_Durum.ForeColor = Color.Green;
+                else
+                    Txt_Durum.ForeColor = Color.Red;
+
+                Txt_Durum.Text = sonuc.msg;
+            }
+        }
+
+        //Bir sözcükte düz ünlü harflerden(a, e, ı, i) sonra 
+        //yine düz ünlü harfler(a, e, ı, i) gelebilir.
+        //Yani düz ünlülerden sonra yuvarlak ünlüler(o, ö, u, ü) gelemez.
+        //Gelirse bu sözcük küçük ünlü uyumuna uymaz
+        // Kelimenin ilk hecesinde yuvarlak sesli harf (o,ö,u,ü) varsa, 
+        //diğer hecelerinde ya düz-geniş (a,e), ya da dar-yuvarlak (u,ü) sesli harf bulunması gerekir
+        //tek hecelide küçük ünlü uyumu aranmaz
+        private Sonuc KucukUnluUyumunaBak(String kelime)
+        {
+            Sonuc sonuc = KelimeKontrol(kelime);
+            if (!sonuc.sonuc)
+                return sonuc;
+
+            sonuc = IstisnaKontrol(kelime);
+            if (!sonuc.sonuc)
+                return sonuc;
+
+            sonuc = new Sonuc();
+
+            Boolean ilkHarfCtrl = false,
+                ilkSesliHarfDuzMu = false,
+                ilkSesliHarfYuvarlakMi = false,
+                ilkSesliHarfMi = false;
+
+            TurkceDilBilgisi dilBilgisi = new TurkceDilBilgisi(new TurkiyeTurkcesi());
+            Alfabe alfabe = dilBilgisi.alfabe();
+
+            List<Char> harfler = kelime.ToList();
+            Boolean uyma = false;
+            for (int i = 0; i < harfler.Count; i++)
+            {
+                TurkceHarf simdikiSesliHarf = alfabe.harf(harfler[i]);
+                if (!simdikiSesliHarf.sesliMi())
+                    continue;
+                if (!ilkHarfCtrl)
+                {
+                    if (simdikiSesliHarf.yuvarlakSesliMi())
+                    {
+                        ilkSesliHarfYuvarlakMi = true;
+                        ilkSesliHarfMi = true;
+                    }
+                    else if (simdikiSesliHarf.duzSesliMi())
+                    {
+                        ilkSesliHarfDuzMu = true;
+                    }
+                    else
+                        break;
+
+                    ilkHarfCtrl = true;
+                }
+                if (ilkSesliHarfDuzMu)
+                {
+                    if (simdikiSesliHarf.duzSesliMi())
+                    {
+                        uyma = true;
+                    }
+                    else
+                    {
+                        uyma = false;
+                        break;
+                    }
+                }
+                else if (ilkSesliHarfYuvarlakMi)
+                {
+                    if (ilkSesliHarfMi)
+                    {
+                        ilkSesliHarfMi = false;
+                        continue;
+                    }
+
+                    if (!ilkSesliHarfMi)
+                    {
+                        char c = simdikiSesliHarf.charDeger();
+
+                        if (DuzGenisYadaDarYuvarlakMi(Convert.ToString(c)))
+                        {
+                            uyma = true;
+                        }
+                        else
+                        {
+                            uyma = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            sonuc.sonuc = uyma;
+            if (uyma)
+                sonuc.msg ="'"+ kelime+ "' kelimesi küçük ünlü uyumuna uyar.";
+            else
+                sonuc.msg = "'" + kelime + "' kelimesi küçük ünlü uyumuna uymaz.";
+
+            return sonuc;
+        }
+
+
+        private Sonuc IstisnaKontrol(String kelime)
+        {
+            Sonuc sonuc = new Sonuc();
+
+            if (zemberek.hecele(kelime).Length > 1)
+                sonuc.sonuc = true;
+            else
+            {
+                sonuc.sonuc = false;
+                sonuc.msg = ("Tek heceli kelimelerde küçük ünlü uyumu aranmaz.");
+            }
+
+            return sonuc;
+        }
+
+
+        private bool DuzGenisYadaDarYuvarlakMi(String harf)
+        {
+            String ha = "aeuüAEUÜ";
+
+            return ha.Contains(harf);
+        }
+    }
+
+    internal class Sonuc
+    {
+        public Boolean sonuc;
+        public String msg;
+    }
+}
+/*
+Heceleyici heceleyici = zemberek.heceleyici();
+            Boolean b = heceleyici.hecelenebilirmi(txt_kelimeGir.Text);
+            if (!zemberek.kelimeDenetle(txt_kelimeGir.Text.ToString()))
+            {
+                MessageBox.Show(txt_kelimeGir.Text.ToString() + " kelimesi dogru yazilmamis");
+                return false;
+            }
+            else if (!b)
+            {
+                MessageBox.Show("Hecelenemez");
+                return false;
+            }
+            return true;
+*/
